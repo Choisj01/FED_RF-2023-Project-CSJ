12e1c1a1275f8aacf0eb5e1b9f0f01e7a396b4ea
Choisj01 <140689624+Choisj01@users.noreply.github.com>
2023-12-04 22:30:52 +0900

    Update core.js

--- 02.second_PJ_CSJ/03.구현코드/js/core.js
+++ 02.second_PJ_CSJ/03.구현코드/js/core.js
@@ -161,6 +161,42 @@ if(pgNm==="index"||pgNm==="sub1_petsochic"||pgNm==="sub2_maxbone"||pgNm==="sub3_
 //   })
 // })
 
+//탑버튼 클릭시 상단이동 
+//탑버튼 클릭이벤트
+document.addEventListener("DOMContentLoaded", () => {
+  let btt = document.querySelector("#go-top");
+  // 문서 자체로 스크롤 양 확인
+  // document.documentElement.scrollTop
+  // 윈도우 기준 페이지 스크롤 양 확인
+  // window.pageYOffset
+  let scrollAmt;
+
+  // 윈도우에 이벤트 스크롤 발생시 할일 함수
+  window.addEventListener("scroll", () => {
+      scrollAmt = window.pageYOffset;
+      console.log(scrollAmt);
+
+      if (scrollAmt > 300) {
+          btt.classList.add("active");
+          // btt.className='active';
+      }
+      else{
+          btt.classList.remove("active");
+      }
+  }); ////////// scroll //////////
+
+  btt.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+      });
+  });
+}); //////////////////로드 구역 //////////////
+
+
+
 // 하단영역 회사정보 클릭시 보이고 닫기
 $('.footer-info-txt').hide();
 
