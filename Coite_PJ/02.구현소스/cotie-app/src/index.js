40ecd4772f6d9c7828d92d7dbe7cda2d362a5ee2
Choisj01 <choisujin96@naver.com>
2023-12-20 17:34:50 +0900

    dd

--- Coite_PJ/02.구현소스/cotie-app/src/index.js
+++ Coite_PJ/02.구현소스/cotie-app/src/index.js
@@ -2,6 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+
+//
+
 import { Main } from "./pages/Main";
 import { Layout } from "./layout/Layout";
 import { Sub1Petsochic } from "./pages/Sub1Petsochic";
