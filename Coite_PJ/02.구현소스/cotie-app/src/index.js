b91ad7c7a31883df2a76a4d665d6982c13a80afb
Choisj01 <choisujin96@naver.com>
2023-12-22 18:00:30 +0900

    Update index.js

--- Coite_PJ/02.구현소스/cotie-app/src/index.js
+++ Coite_PJ/02.구현소스/cotie-app/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 
-//
+
 
 import { Main } from "./pages/Main";
 import { Layout } from "./layout/Layout";
